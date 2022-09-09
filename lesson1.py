92d654715a9b5ae2d5b0a79a5d3b8a1fbdf62422
Mahad <56235065+Mahad-lab@users.noreply.github.com>
2022-09-09 16:26:53 +0500

    Lesson 1

---
+++ lesson1.py
@@ -0,0 +1,37 @@
+# - COMMENTS
+# hashtag are used for comments in python
+
+# primary datatypes
+
+print(123)             #integer
+print(12.5)            #float
+print('Hello World')   #string
+print("Hello World")   #string
+
+# simple aritmatic operations
+
+# addition
+print(2 + 2)
+# substraction
+print(5 - 2)
+# multiplication
+print(4 * 2)
+# divisions
+# divisions always return float
+print(8 / 4)
+
+# Modulo (Remainder) 
+print(22 % 5)  
+# Floor Division (Quotient)
+print(22 // 5)
+
+# exponent (Powers)
+print(3**3)
+# solution: 3*3*3 = 27
+
+
+# # 25 Apples
+# # 4 child
+
+# print(25 % 4)
+# print(25 // 4)
